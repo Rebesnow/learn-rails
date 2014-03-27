10b9c6b3a9f4bf0b34e6fdf1f537e753acd5ba1c
Rebeca Polo <rebesnow@gmail.com>
2014-03-27 22:58:17 +0100

    dynamic home page

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,7 @@
+class VisitorsController < ApplicationController #We define the class and name it class VisitorsController , inheriting behavior from the
+												#ApplicationController class which is defined in the Rails API.
+	def new
+		@owner = Owner.new
+		render 'visitors/new'
+	end
+end
---
+++ app/models/owner.rb
@@ -0,0 +1,26 @@
+# we'll create a Ruby class with methods that return the owner's name, birthdate, and days remaining until your birthday.
+
+
+class Owner # this line defines the class and assigns a name.
+
+def name # define method 1
+name = 'Foobar Kadigan' # ruby recognizes a string when it is btw ' '
+end
+
+def birthdate # define method 2
+birthdate = Date.new(1990, 12, 22) #same synthax in line 
+end
+
+def countdown # define method 3 #most complex code in the class
+today = Date.today # gets the date from the PC clock
+birthday = Date.new(today.year, birthdate.month, birthdate.day) # date class and new method that instantiates a new date
+	if birthday > today
+	countdown = (birthday - today).to_i
+	else
+	countdown = (birthday.next_year - today).to_i 
+	end
+end
+
+end # the end keyword completes the class definition
+
+# ruby has specific expectations about the syntax of numbers. The Date.new expects integers. if we introduce (1990,  09, 22) we'll get an error
